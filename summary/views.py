2b42f93382ff77eb1f561f80cf410e9ebc6d30a4
EngenerPaul <kpashka@mail.ru>
2022-01-28 06:09:49 +0400

    create view

--- summary/views.py
+++ summary/views.py
@@ -1,10 +1,30 @@
+from calendar import month
+from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+
+from .models import Course, PetProject
 
 # Create your views here.
-def index(request):
-    template = 'summary/index.html'
-    context = {
-        'my_photo': 'ICY-mAf5sYY.jpg',
-    }
-    return render(request, template_name=template, context=context)
+# def index(request):
+#     template = 'summary/index.html'
+#     context = {
+#         'my_photo': 'ICY-mAf5sYY.jpg',
+#     }
+#     return render(request, template_name=template, context=context)
+
+class Home(ListView):
+    model = Course
+    template_name = 'summary/index.html'
+    context_object_name = 'courses'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pet_projects'] = PetProject.objects.all()
+        context['date_of_birth'] = date(year=1996, month=5, day=18)
+        context['age'] = 25 # переделать!!
+        context['my_photo'] = 'ICY-mAf5sYY.jpg'
+        context['stack'] = ('Python 3', 'Django', 'PostgreSQL', 'GIT', 'html, css (базовый уровень)', )
+        return context
+
