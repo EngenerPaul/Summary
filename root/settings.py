73bc28846a616a54272b630f3e7ecad1d337b1e3
EngenerPaul <kpashka@mail.ru>
2022-02-19 22:08:59 +0400

    was created my_age func. was changed index and footer

--- root/settings.py
+++ root/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Samara'
 
 USE_I18N = True
 
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'root/static'),
 ]
--- summary/views.py
+++ summary/views.py
@@ -1,4 +1,3 @@
-from calendar import month
 from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -6,13 +5,20 @@ from django.views.generic import ListView
 
 from .models import Course, PetProject
 
-# Create your views here.
-# def index(request):
-#     template = 'summary/index.html'
-#     context = {
-#         'my_photo': 'ICY-mAf5sYY.jpg',
-#     }
-#     return render(request, template_name=template, context=context)
+
+def my_age():
+    """this function shows my age today"""
+
+    today = date.today()
+    birthday = date(year=today.year, month=5, day=18)
+    birthdate = date(year=1996, month=5, day=18)
+    
+    age = today.year - birthdate.year
+    if today >= birthday:
+        return age
+    else:
+        return age - 1
+
 
 class Home(ListView):
     model = Course
@@ -23,8 +29,10 @@ class Home(ListView):
         context = super().get_context_data(**kwargs)
         context['pet_projects'] = PetProject.objects.order_by('id')
         context['date_of_birth'] = date(year=1996, month=5, day=18)
-        context['age'] = 25 # переделать!!
+        context['age'] = my_age()
         context['my_photo'] = 'ICY-mAf5sYY.jpg'
         context['stack'] = ('Python 3', 'Django', 'PostgreSQL', 'GIT', 'html, css (базовый уровень)', )
         return context
+    
+    
 
