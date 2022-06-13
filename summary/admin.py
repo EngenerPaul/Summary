edba54f9e22aae82986a7ac896ce2e186d9da275
EngenerPaul <kpashka@mail.ru>
2022-06-13 16:36:52 +0400

    Change Engineer

--- summary/admin.py
+++ summary/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Course, PetProject
+from .models import Course, PetProject, TechnologyStack
 
 admin.site.register(Course)
 admin.site.register(PetProject)
+admin.site.register(TechnologyStack)
